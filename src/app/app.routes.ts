e68490b1e9f79d40da3f23b598bf33f3ea81a270
faTimaezzahrarguiti <fatimazahraraguti@gmail.com>
2025-08-03 22:34:49 +0100

    correction erreur

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './auth/register/register.component';
 import { LoginComponent } from './auth/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { ListCompetenceComponent } from './components/list-competence/list-competence.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -10,4 +11,6 @@ export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
+    { path: 'list-competence', component: ListCompetenceComponent}
+
 ];
---
+++ src/app/components/list-competence/list-competence.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import {MatTableModule} from '@angular/material/table';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
+@Component({
+  selector: 'app-list-competence',
+  standalone: true,
+    imports: [
+    MatTableModule,
+    MatToolbarModule
+  ],
+  templateUrl: './list-competence.component.html',
+  styleUrl: './list-competence.component.css'
+})
+export class ListCompetenceComponent {
+ displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
+}
